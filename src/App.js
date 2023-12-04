7a90b7fe0b346802457b6d9090c962b205aebf9a
73772114189 <samuvelraja072@gmail.com>
2023-12-04 04:06:18 -0500

    newapp commited

--- src/App.js
+++ src/App.js
@@ -4,20 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+    <li>Name : Samuvel raja V.</li> 
+    <li> Department: CSE.</li> 
+     <li>Year: 3rd Year.</li> 
+     <li>Register number: 73772114189.</li> 
+     <li>Area of Interest: Full stack web development.</li>
+     </ul>
     </div>
   );
 }
